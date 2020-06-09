04db25337688757915217ab5dff15886697d44d2
Gabriel <joaogabrielonofre@gmail.com>
2020-06-08 21:08:29 -0300

    User registration created

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,13 @@
 const express = require('express');
 const router = express.Router();
+//gravatar package
+const gravatar = require('gravatar');
+//bcryopt
+const bcrypt = require('bcryptjs');
+// requiring the express-validator methods that we'll use
 const { check, validationResult } = require('express-validator');
+// importing the user model
+const User = require('../../models/User');
 
 // @route   POST api/users
 // @desc    Register user
@@ -8,6 +15,7 @@ const { check, validationResult } = require('express-validator');
 router.post(
   '/',
   [
+    //this is the middleware with the express-validator logic
     check('name', 'Name is required').not().isEmpty(),
     check('email', 'Please include a valid email').isEmail(),
     check(
@@ -17,12 +25,49 @@ router.post(
       min: 6,
     }),
   ],
-  (req, res) => {
+  async (req, res) => {
+    //handling erros of the express-validator
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
-    res.send('User Route');
+
+    const { name, email, password } = req.body;
+
+    try {
+      //see if the user exists
+      let user = await User.findOne({ email }); //searching the email in db
+      if (user) {
+        return res
+          .status(400)
+          .json({ erros: [{ msg: 'User already exists' }] });
+      }
+      // get users gravatar
+      const avatar = gravatar.url(email, {
+        s: '200', //size
+        r: 'pg', //rate - no pornographic
+        d: 'mm', //default: user icon
+      });
+      //creating a new user
+      user = new User({
+        name,
+        email,
+        avatar,
+        password,
+      });
+      // Encrypt the password
+      const salt = await bcrypt.genSalt(10);
+      user.password = await bcrypt.hash(password, salt); //creates a hash in the user pw
+
+      await user.save();
+
+      // Return the jswonwebtoken
+
+      res.send('User registered');
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server Error');
+    }
   }
 );
 
