385cc81bc89cbada15058f13e11c0dc72e6fa7c1
Gabriel <joaogabrielonofre@gmail.com>
2020-06-10 23:24:03 -0300

    auth middleware created

---
+++ routes/api/middleware/auth.js
@@ -0,0 +1,22 @@
+const jwt = require('jsonwebtoken');
+const config = require('config');
+
+module.exports = function (req, res, next) {
+  //Get the token from the header
+  const token = req.header('x-auth-token'); //header key that we want to send the token on
+
+  //check if no token
+  if (!token) {
+    return res.status(401).json({ msg: 'No token, auth denied' });
+  }
+
+  //Verify token
+  try {
+    const decoded = jwt.verify(token, config.get('jwtSecret'));
+
+    req.user = decoded.user;
+    next();
+  } catch (err) {
+    res.status(401).json({ msg: 'Token is not valid' });
+  }
+};
