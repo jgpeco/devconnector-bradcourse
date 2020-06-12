ac423465ac326dc1b824ab176a2e6dbc740bf103
Gabriel <joaogabrielonofre@gmail.com>
2020-06-11 21:21:57 -0300

    Add new POST route that creates or updates a profile in the profile route file

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const auth = require('../../middleware/auth');
+const { check, validationResult } = require('express-validator');
+
 const User = require('../../models/User');
 const Profile = require('../../models/Profile');
 
@@ -23,5 +25,89 @@ router.get('/me', auth, async (req, res) => {
     res.status(500).send('Server Error');
   }
 });
+
+// @route   POST api/profile/
+// @desc    Create or Update a User Profile
+// @access  Private
+router.post(
+  '/',
+  [
+    auth,
+    [
+      //express validator checking for required fields
+      check('status', 'Status is required').not().isEmpty(),
+      check('skills', 'Skills is requrired').not().isEmpty(),
+    ],
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    } //express-validator checking for errors
+
+    const {
+      company,
+      website,
+      location,
+      bio,
+      status,
+      githubusername,
+      skills,
+      youtube,
+      facebook,
+      twitter,
+      instagram,
+      linkedin,
+    } = req.body; //destructuring the profile data received from form
+
+    //Build profile object
+    const profileFields = {};
+    profileFields.user = req.user.id;
+    if (company) profileFields.company = company;
+    if (website) profileFields.website = website;
+    if (location) profileFields.location = location;
+    if (bio) profileFields.bio = bio;
+    if (status) profileFields.status = status;
+    if (githubusername) profileFields.githubusername = githubusername;
+    if (skills) {
+      profileFields.skills = skills.split(',').map((skill) => skill.trim());
+      //this here is taking every skill spearated with a comma and putting it inside a new array
+    }
+
+    //Build Social Object - We have to initialize it first,
+    // otherwise we don't have access what is inside the social object
+    profileFields.social = {};
+    if (twitter) profileFields.social.twitter = twitter;
+    if (facebook) profileFields.social.facebook = facebook;
+    if (linkedin) profileFields.social.linkedin = linkedin;
+    if (instagram) profileFields.social.instagram = instagram;
+    if (youtube) profileFields.social.youtube = youtube;
+
+    try {
+      let profile = await Profile.findOne({ user: req.user.id }); //search profile
+
+      if (profile) {
+        //Update
+        profile = await Profile.findOneAndUpdate(
+          { user: req.user.id },
+          { $set: profileFields },
+          { new: true }
+        );
+
+        return res.json(profile);
+      }
+
+      //Create
+      profile = new Profile(profileFields);
+      await profile.save();
+      res.json(Profile);
+
+      //handle errors
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server Error');
+    }
+  }
+);
 
 module.exports = router;
