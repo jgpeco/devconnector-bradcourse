efd94ef93fa877f5ecb01a553454888f9f135c11
Gabriel <joaogabrielonofre@gmail.com>
2020-06-14 19:07:01 -0300

    Delete Route add to the profile router

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -146,4 +146,23 @@ router.get('/user/:user_id', async (req, res) => {
   }
 });
 
+// @route   DELETE api/profile/
+// @desc    Delete a profile, user and posts
+// @access  Private
+
+router.delete('/', auth, async (req, res) => {
+  try {
+    // @todo - remove users posts
+    //remove profile
+    await Profile.findOneAndRemove({ user: req.user.id });
+    //remove the user
+    await User.findOneAndRemove({ _id: req.user.id });
+
+    res.json({ msg: 'User removed' });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
+
 module.exports = router;
