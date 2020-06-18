741d37a0e5e0c421522a6923d9b93896b958934f
Gabriel <joaogabrielonofre@gmail.com>
2020-06-18 20:41:39 -0300

    Created Register form with react and useState

--- client/src/components/layout/auth/Register.js
+++ client/src/components/layout/auth/Register.js
@@ -1,6 +1,27 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
 
 const Register = () => {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    password: '',
+    password2: '',
+  });
+
+  const { name, email, password, password2 } = formData;
+
+  const onChange = (e) =>
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    if (password !== password2) {
+      console.log('Password do not match');
+    } else {
+      console.log(formData);
+    }
+  };
+
   return (
     <Fragment>
       <section className='container'>
@@ -8,12 +29,29 @@ const Register = () => {
         <p className='lead'>
           <i className='fas fa-user'></i> Create Your Account
         </p>
-        <form className='form' action='create-profile.html'>
+
+        <form className='form' onSubmit={(e) => onSubmit(e)}>
           <div className='form-group'>
-            <input type='text' placeholder='Name' name='name' required />
+            <input
+              type='text'
+              placeholder='Name'
+              name='name'
+              value={name}
+              onChange={(e) => onChange(e)}
+              required
+            />
           </div>
+
           <div className='form-group'>
-            <input type='email' placeholder='Email Address' name='email' />
+            <input
+              type='email'
+              placeholder='Email Address'
+              name='email'
+              value={email}
+              onChange={(e) => onChange(e)}
+              required
+            />
+
             <small className='form-text'>
               This site uses Gravatar so if you want a profile image, use a
               Gravatar email
@@ -25,6 +63,8 @@ const Register = () => {
               placeholder='Password'
               name='password'
               minLength='6'
+              value={password}
+              onChange={(e) => onChange(e)}
             />
           </div>
           <div className='form-group'>
@@ -33,6 +73,8 @@ const Register = () => {
               placeholder='Confirm Password'
               name='password2'
               minLength='6'
+              value={password2}
+              onChange={(e) => onChange(e)}
             />
           </div>
           <input type='submit' className='btn btn-primary' value='Register' />
