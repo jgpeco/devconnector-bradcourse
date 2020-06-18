4520658b974aa13d81cd9b2ab89cb012a8113280
Gabriel <joaogabrielonofre@gmail.com>
2020-06-18 13:48:35 -0300

    Fixed unlike route error in post router

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -138,9 +138,8 @@ router.put('/unlike/:id', auth, async (req, res) => {
 
     //check if the post has already been liked by this user
     if (
-      (post.likes.filter(
-        (like) => like.user.toString() === req.user.id
-      ).length = 0) //this time we are checking if the post isn't liked
+      post.likes.filter((like) => like.user.toString() === req.user.id)
+        .length === 0 //this time we are checking if the post isn't liked
     ) {
       return res.status(400).json({ msg: 'Post has not yet been liked' });
     }
