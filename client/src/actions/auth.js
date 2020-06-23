0dd0a4a00bad64188f32fd51b129acffb2fb6a00
Gabriel <joaogabrielonofre@gmail.com>
2020-06-22 21:22:35 -0300

    Fixed calling of load user in the App.js

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -15,7 +15,7 @@ export const loadUser = () => async (dispatch) => {
   }
 
   try {
-    const res = await axios.get('/api/auth');
+    const res = await axios.get('/auth');
 
     dispatch({
       type: USER_LOADED,
