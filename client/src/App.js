c4de1f991dfb0086a13f76a31691609bbcd997f5
Gabriel <joaogabrielonofre@gmail.com>
2020-06-23 19:44:15 -0300

    Created Private Route setup

--- client/src/App.js
+++ client/src/App.js
@@ -2,9 +2,11 @@ import React, { Fragment, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
+import PrivateRoute from './components/routing/PrivateRoute';
 import Alert from './components/layout/Alert';
 import Login from './components/auth/Login';
 import Register from './components/auth/Register';
+import Dashboard from './components/dashboard/Dashboard';
 // Redux
 import { Provider } from 'react-redux';
 import store from './store';
@@ -33,6 +35,7 @@ const App = () => {
             <Switch>
               <Route exact path='/register' component={Register} />
               <Route exact path='/login' component={Login} />
+              <PrivateRoute exact path='/dashboard' component={Dashboard} />
             </Switch>
           </section>
         </Fragment>
