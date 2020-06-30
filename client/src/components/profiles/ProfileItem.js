5b1970ee76bb9f93491731950a5ac056163090c0
Gabriel <joaogabrielonofre@gmail.com>
2020-06-29 21:06:38 -0300

    Created main component of the Profile section of the app

--- client/src/components/profiles/ProfileItem.js
+++ client/src/components/profiles/ProfileItem.js
@@ -12,23 +12,23 @@ const ProfileItem = ({
   },
 }) => {
   return (
-    <div class='profile bg-light'>
-      <img class='round-img' src={avatar} alt='' />
+    <div className='profile bg-light'>
+      <img className='round-img' src={avatar} alt='' />
       <div>
         <h2>{name}</h2>
         <p>
           {status} {company && <span>at {company}</span>}
         </p>
         <p className='my-1'>{location && <span>{location}</span>}</p>
-        <Link to={`/profile/${_id}`} class='btn btn-primary'>
+        <Link to={`/profile/${_id}`} className='btn btn-primary'>
           View Profile
         </Link>
       </div>
 
       <ul>
         {skills.slice(0, 4).map((skill, index) => (
-          <li key={index} class='text-primary'>
-            <i class='fas fa-check'></i> {skill}
+          <li key={index} className='text-primary'>
+            <i className='fas fa-check'></i> {skill}
           </li>
         ))}
       </ul>
