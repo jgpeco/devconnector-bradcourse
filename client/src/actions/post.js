a6ff269550819e3d07c0fa07062cf4215cf32743
Gabriel <joaogabrielonofre@gmail.com>
2020-07-02 21:35:34 -0300

    Created PostItem component

--- client/src/actions/post.js
+++ client/src/actions/post.js
@@ -2,8 +2,7 @@ import axios from 'axios';
 import { setAlert } from './alert';
 import { GET_POSTS, POST_ERROR } from './types';
 
-//GEt Posts
-
+//Get Posts
 export const getPosts = () => async (dispatch) => {
   try {
     const res = await axios.get('/api/posts');
