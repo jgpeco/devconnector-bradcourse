a1d0c41eef3905acafb4ac307a0780bc8c72e694
Gabriel <joaogabrielonofre@gmail.com>
2020-07-02 22:36:05 -0300

    Created new action and new component form to Add a new Post

--- client/src/reducers/post.js
+++ client/src/reducers/post.js
@@ -3,6 +3,7 @@ import {
   POST_ERROR,
   UPDATE_LIKES,
   DELETE_POST,
+  ADD_POST,
 } from '../actions/types';
 
 const initialState = {
@@ -22,6 +23,13 @@ export default function (state = initialState, action) {
         posts: payload,
         loading: false,
       };
+    case ADD_POST: {
+      return {
+        ...state,
+        posts: [payload, ...state.posts],
+        loading: false,
+      };
+    }
     case DELETE_POST:
       return {
         ...state,
